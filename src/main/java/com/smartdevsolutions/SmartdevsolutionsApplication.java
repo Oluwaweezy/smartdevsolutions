00838c8a3b063dc6a48dd9419a6176df218df977
BinaryCodes <ayoolakelani@yahoo.com>
2017-11-06 16:40:21 +0100

    Initial commit for first spring boot project

---
+++ src/main/java/com/smartdevsolutions/SmartdevsolutionsApplication.java
@@ -0,0 +1,12 @@
+package com.smartdevsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdevsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartdevsolutionsApplication.class, args);
+	}
+}
