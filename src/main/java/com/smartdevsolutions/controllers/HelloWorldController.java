bd796870d37f0cd4ec80e07de63b7da3d7961486
BinaryCodes <ayoolakelani@yahoo.com>
2017-11-06 16:58:10 +0100

    Added HelloWorld Index Page

---
+++ src/main/java/com/smartdevsolutions/controllers/HelloWorldController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.smartdevsolutions.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author BinaryCodes_2
+ *
+ */
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String SayHello()
+	{
+		return "index";
+	}
+
+}
