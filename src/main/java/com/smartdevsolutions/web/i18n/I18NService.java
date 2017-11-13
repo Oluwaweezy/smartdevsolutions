3a8a3c3c77115bab41eecfc64b823f2640836163
BinaryCodes <ayoolakelani@yahoo.com>
2017-11-13 16:34:30 +0100

    Added logging with slf4j

--- src/main/java/com/smartdevsolutions/web/i18n/I18NService.java
+++ src/main/java/com/smartdevsolutions/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.smartdevsolutions.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +15,11 @@ public class I18NService {
     @Autowired
  public MessageSource messageSource;
 
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class); 
+    
     public String getMessage(String messageId)
     {
+        LOG.info("returning 18n text from messageId {}",messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return  getMessage(messageId, locale);
     }
