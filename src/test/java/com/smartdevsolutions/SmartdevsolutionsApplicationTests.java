dc0ea40df8c35a9132a88a2a293c6fe41cc44264
BinaryCodes <ayoolakelani@yahoo.com>
2017-11-13 15:49:16 +0100

    Added internationalization

--- src/test/java/com/smartdevsolutions/SmartdevsolutionsApplicationTests.java
+++ src/test/java/com/smartdevsolutions/SmartdevsolutionsApplicationTests.java
@@ -1,7 +1,10 @@
 package com.smartdevsolutions;
 
+import com.smartdevsolutions.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SmartdevsolutionsApplicationTests {
 
-	@Test
+/*	@Test
 	public void contextLoads() {
+	}*/
+
+
+	 @Autowired
+	private I18NService i18NService;
+
+	@Test
+	 public void testMessageByLocaleService() throws  Exception
+	{
+      String expectedResult = "Bootstrap starter template";
+      String messageID = "index.main.callout";
+      String actual = i18NService.getMessage(messageID);
+		Assert.assertEquals("The actual Expected String Don't match", expectedResult,actual);
 	}
 
 }
